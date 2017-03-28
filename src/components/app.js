59d452ad761d921a047dc3767bab37b11b5ecf8d
jamesv28 <james.volmert@davita.com>
2017-03-28 14:32:38 -0600

    working on features

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../container/searchbar';
-
+import WeatherList from '../container/weather_list';
 export default class App extends Component {
   render() {
     return (
       <div>
         <div>React simple starter</div>
         < SearchBar />
+      < WeatherList />
       </div>
     );
   }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // try not to manipulate state - we create new arrays
+      return [action.payload.data, ...state];  // new entries go to the top
+  }
+
   return state;
 }
